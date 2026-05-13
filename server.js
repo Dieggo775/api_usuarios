b1719b747fc263c544043a558f37449891b4b38e
Diego Araujo <dyeggotaylor@gmail.com>
2026-05-13 08:59:33 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,33 @@
+import express from 'express'
+
+const app = express()
+app.use(express.json())
+
+const users = []
+
+app.post('/usuarios' , (req, res) => {
+
+    users.push(req.body)
+
+    res.status(201).json(req.body)
+})
+
+app.get('/usuarios', (req, res) => {
+  res.status(200).json(users)
+})
+
+app.listen(3000)
+
+/*
+ Criar nossa API de usuarios
+
+ - Criar um usuário
+ - Listar todos os usuários
+ - Editar um usuário
+ - Deletar um usuário
+
+ Mongo DB
+ db_user
+ eCzTR160Z7K3VicZ
+
+*/
