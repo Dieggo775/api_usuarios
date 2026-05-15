46bde4ccb0017dcc8ff8550b3a78a6ed6d3c64a2
Diego Araujo <dieggo_taylor@hotmail.com>
2026-05-15 16:27:41 -0300

    atualização

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient({})
 
 const app = express()
 app.use(express.json())
 
 const users = []
 
-app.post('/usuarios' , (req, res) => {
+app.post('/usuarios', async (req, res) => {
+  const user = await prisma.user.create({
+    data: {
+      email: req.body.email,
+      name: req.body.name,
+      age: req.body.age,
+    },
+  })
 
-    users.push(req.body)
+  users.push(req.body)
 
-    res.status(201).json(req.body)
+  res.status(201).json(user)
 })
 
 app.get('/usuarios', (req, res) => {
